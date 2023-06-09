96b192817d7bb5a1828450e752fb67bfc3971a4c
kitewatermelon <parkite576@gmail.com>
2023-06-09 17:32:27 +0900

    arrow function 추가

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const pokemon = require('pokemon');
+document.addEventListener('DOMContentLoaded', function() {
+    var grayTop = document.querySelector('.graytop');
+    var grayBot = document.querySelector('.graybot');
+    var textBox = document.querySelector('.textBox');
 
-pokemon.all();
-//=> ['Bulbasaur', …]
+    grayTop.addEventListener('click', function() {
+        this.style.transform = 'rotate(60deg)';
+        setTimeout(function() {
+            window.location.href = 'sticker2.html';
+        }, 1000); // 3초 후에 sticker.html로 이동
+    });
 
-console.log(pokemon.random('ko'));
-//=> 'Snorlax'
+    grayBot.addEventListener('click', function() {
+        grayTop.style.transform = 'rotate(60deg)';
+        setTimeout(function() {
+            window.location.href = 'sticker2.html';
+        }, 3000); // 3초 후에 sticker.html로 이동
+    });
+});
 
-console.log(pokemon.getName(100 , 'ko'));
-//=> 'Dratini'
+var arrow = document.querySelector('.arrow');
 
-console.log(pokemon.getId('Dratini'));
-//=> 147
-
+setInterval(function() {
+    arrow.innerText = '▽';
+}, 500); // 0.5초마다 내용을 ▽로 변경
+setInterval(function() {
+    arrow.innerText = '▼';
+}, 1000); // 0.5초마다 내용을 ▽로 변경
